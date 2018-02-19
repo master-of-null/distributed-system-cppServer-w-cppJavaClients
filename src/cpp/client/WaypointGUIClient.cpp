75413c96af61e545044cd408b40cb77e37b309ee
Gabo Martinez <gabmart02@gmail.com>
2018-02-19 01:30:26 -0500

    adding arguments for host and port for command line

--- src/cpp/client/WaypointGUIClient.cpp
+++ src/cpp/client/WaypointGUIClient.cpp
@@ -56,7 +56,7 @@
 
 
 class Client : public WaypointGUI {
-  jsonrpc::HttpClient httpclient("http://127.0.0.1:8080")
+  jsonrpc::HttpClient httpclient("http://127.0.0.1:8080");
   static waypointlibrarystub wc(httpclient);
    /** ClickedX is one of the callbacks for GUI controls.
     * Callbacks need to be static functions. But, static functions
